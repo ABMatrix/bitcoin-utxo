3903de25c22b56071163a7e731a9932bb492be47
Nathan Yang <nathan.yang@usask.ca>
2021-12-16 19:47:39 +0800

    version

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ import (
 
 // Version
 const (
-	Version                     = "test-8"
+	Version                     = "test-9"
 	ENV_MONGO_URI               = "MONGO_URI"
 	ENV_MONGO_BITCOIN_DB_NAME   = "MONGO_UTXO_DB_NAME"
 	UTXO_COLLECTION_NAME_PREFIX = "utxo"
