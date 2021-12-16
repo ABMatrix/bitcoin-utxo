3e6f13ff39b5a3504f14b5d18bd1581c8ed275d8
Nathan Yang <nathan.yang@usask.ca>
2021-12-16 19:36:29 +0800

    bump up buf size again

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ const (
 	ENV_MONGO_URI               = "MONGO_URI"
 	ENV_MONGO_BITCOIN_DB_NAME   = "MONGO_UTXO_DB_NAME"
 	UTXO_COLLECTION_NAME_PREFIX = "utxo"
-	BUF_SIZE                    = 1 << 13
+	BUF_SIZE                    = 1 << 20
 
 	NONSTANDARD           string = "nonstandard"
 	PUBKEY                string = "pubkey"
