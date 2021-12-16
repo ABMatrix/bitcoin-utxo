d05bac31f007de63ce109d56afc9a3f0484ae93f
Nathan Yang <nathan.yang@usask.ca>
2021-12-16 19:15:54 +0800

    revert

--- main.go
+++ main.go
@@ -182,7 +182,7 @@ func main() {
 
 		if prefix != 0x43 {
 			log.Printf("[warning] unexpected prefix: %#x at %d\n", prefix, entries)
-			break
+			continue
 		}
 
 		utxo, err := processEachEntry(key, value, obfuscateKey, testnet)
