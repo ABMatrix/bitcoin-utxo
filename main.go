24f74780acc8676a099183064ab4251657cad7aa
Nathan Yang <nathan.yang@usask.ca>
2021-12-20 11:55:56 +0800

    use mongo transaction

--- main.go
+++ main.go
@@ -6,12 +6,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 	"syscall"
-	"time"
 
 	"github.com/ABMatrix/bitcoin-utxo/bitcoin/bech32"
 	"github.com/ABMatrix/bitcoin-utxo/bitcoin/btcleveldb"
@@ -26,13 +25,17 @@ import (
 
 // Version
 const (
-	Version                     = "beta-7"
+	Version                     = "beta-8"
 	ENV_MONGO_URI               = "MONGO_URI"
 	ENV_MONGO_BITCOIN_DB_NAME   = "MONGO_UTXO_DB_NAME"
 	UTXO_COLLECTION_NAME_PREFIX = "utxo"
 	BUF_SIZE                    = 1 << 20
-	RETRY_INTERVAL              = 10 * time.Second
-	MAX_RETRIES                 = 5
+)
+
+var (
+	utxoCollectionName string
+	mongoDBName        string
+	mongoCli           *mongo.Client
 )
 
 func main() {
@@ -55,7 +58,7 @@ func main() {
 
 	// Mainnet or Testnet (for encoding addresses correctly)
 	testnet := false
-	utxoCollectionName := UTXO_COLLECTION_NAME_PREFIX + "-mainnet"
+	utxoCollectionName = UTXO_COLLECTION_NAME_PREFIX + "-mainnet"
 	if *testnetFlag || strings.Contains(*chainstate, "testnet") { // check testnet flag
 		testnet = true
 		utxoCollectionName = UTXO_COLLECTION_NAME_PREFIX + "-testnet"
@@ -108,7 +111,7 @@ func main() {
 		log.Fatalln("[fatal] mongo URI is unset")
 	}
 
-	mongoDBName := os.Getenv(ENV_MONGO_BITCOIN_DB_NAME) // "bitcoin"
+	mongoDBName = os.Getenv(ENV_MONGO_BITCOIN_DB_NAME) // "bitcoin"
 	if mongoDBName == "" {
 		log.Fatalln("[fatal] mongo db name is unset")
 	}
@@ -117,10 +120,11 @@ func main() {
 	clientOptions := options.Client().ApplyURI(mongoURI)
 
 	// connect to MongoDB
-	mongoCli, err := mongo.Connect(ctx, clientOptions)
+	mongoCli, err = mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatalln("[fatal] failed to connect with error:", err)
 	}
+	defer mongoCli.Disconnect(ctx)
 	// check connection
 	err = mongoCli.Ping(ctx, nil)
 	if err != nil {
@@ -128,8 +132,6 @@ func main() {
 	}
 
 	log.Println("[info] mongo connection is OK...")
-	utxoDB := mongoCli.Database(mongoDBName)
-	utxoCollection := utxoDB.Collection(utxoCollectionName)
 
 	// Catch signals that interrupt the script so that we can close the database safely (hopefully not corrupting it)
 	c := make(chan os.Signal, 1)
@@ -138,9 +140,8 @@ func main() {
 		<-c // receive from channel
 		log.Println("Interrupt signal caught. Shutting down gracefully.")
 		// iter.Release() // release database iterator
-		db.Close()               // close database
-		utxoCollection.Drop(ctx) // drop collection
-		os.Exit(0)               // exit
+		db.Close() // close database
+		os.Exit(0) // exit
 	}()
 
 	// get obfuscate key (a byte slice)
@@ -156,12 +157,12 @@ func main() {
 	var count int64
 	var entries int64
 	var utxoBuf []*UTXO
+	wg := &sync.WaitGroup{}
 	for ok := iter.Seek([]byte{0x43}); ok; ok = iter.Next() {
 		entries++
 
-		key := make([]byte, len(iter.Key()))
+		key, value := make([]byte, len(iter.Key())), make([]byte, len(iter.Value()))
 		copy(key, iter.Key())
-		value := make([]byte, len(iter.Value()))
 		copy(value, iter.Value())
 
 		utxo, err := processEachEntry(key, value, obfuscateKey, testnet)
@@ -175,16 +176,31 @@ func main() {
 			continue
 		}
 		totalAmount += utxo.Amount
+		if len(utxoBuf) == BUF_SIZE {
+			// convert to mongo-acceptable arguments...
+			var docs []interface{}
+			for _, utxo := range utxoBuf {
+				docs = append(docs, utxo)
+			}
+			utxoBuf = make([]*UTXO, 0)
+			wg.Add(1)
+			go insertUTXO(ctx, docs, wg)
+		}
 		utxoBuf = append(utxoBuf, utxo)
 		count++
 	}
 	iter.Release() // Do not defer this, want to release iterator before closing database
 
-	for len(utxoBuf) > 0 {
-		size := int(math.Min(float64(len(utxoBuf)), float64(BUF_SIZE)))
-		insertUTXO(ctx, utxoBuf[:size], utxoCollection)
-		utxoBuf = utxoBuf[size:]
+	if len(utxoBuf) > 0 {
+		var docs []interface{}
+		for _, utxo := range utxoBuf {
+			docs = append(docs, utxo)
+		}
+		wg.Add(1)
+		go insertUTXO(ctx, docs, wg)
 	}
+
+	wg.Wait()
 
 	// Final Report
 	// ---------------------
@@ -434,27 +450,34 @@ func processEachEntry(key []byte, value []byte, obfuscateKey []byte, testnet boo
 	return output, nil
 }
 
-func insertUTXO(ctx context.Context, buf []*UTXO, utxoCollection *mongo.Collection) {
-	log.Printf("[info] inserting %d utxo...\n", len(buf))
-	// convert to mongo-acceptable arguments...
-	var docs []interface{}
-	for _, utxo := range buf {
-		docs = append(docs, utxo)
-	}
-	var err error
+func insertUTXO(ctx context.Context, docs []interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-	retried := 0
-	for _, err = utxoCollection.InsertMany(ctx, docs); err != nil && retried < MAX_RETRIES; _, err = utxoCollection.InsertMany(ctx, docs) {
-		retried++
-		log.Println("[error] failed to insert many with error: ", err.Error())
-		log.Printf("[info] start retrying at attempt #%d in %s\n", retried, RETRY_INTERVAL.String())
-		time.Sleep(RETRY_INTERVAL)
-	}
-	if err != nil && retried >= MAX_RETRIES {
-		log.Println("[error] max retries reached, giving up now...")
-		syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
-		time.Sleep(time.Second)
+	if err := mongoCli.UseSession(ctx, func(sessionContext mongo.SessionContext) (err error) {
+		if err = sessionContext.StartTransaction(); err != nil {
+			log.Println("[error] failed to start transaction with error: ", err.Error())
+			return err
+		}
+		defer sessionContext.EndSession(ctx)
+
+		log.Printf("[info] inserting %d utxo...\n", len(docs))
+
+		if _, err = mongoCli.Database(mongoDBName).Collection(utxoCollectionName).InsertMany(ctx, docs); err != nil {
+			log.Println("[error] failed to insert many with error: ", err.Error())
+			if e := sessionContext.AbortTransaction(sessionContext); e != nil {
+				log.Println("[error] failed to abort transaction with error: ", e.Error())
+			}
+			return err
+		}
+
+		if err = sessionContext.CommitTransaction(ctx); err != nil {
+			log.Println("[error] failed to commit transaction with error: ", err.Error())
+			return err
+		}
+		log.Printf("[info] successfully finished inserting %d utxos\n", len(docs))
+		return nil
+	}); err != nil {
+		log.Println("[error] failed to use session with error: ", err.Error())
 		return
 	}
-	log.Printf("[info] finished inserting %d utxos\n", len(buf))
 }
